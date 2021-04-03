18bd6d2035652c1ae8186d6b8f734bf2bf4a7fb6
Rohald van Merode <rohald_89@live.nl>
2021-04-03 15:21:00 +0200

    cleaned up comments

--- src/components/context/index.js
+++ src/components/context/index.js
@@ -17,36 +17,6 @@ export const Provider = (props) => {
     return name;
   }
 
-
-//   const handleScoreChange = (index, delta) => {
-//     setPlayers(prevState => {
-//       const updatedPlayers = [ ...prevState ];
-//       const updatedPlayer = { ...updatedPlayers[index] };
-
-//       updatedPlayer.score += delta;
-//       updatedPlayers[index] = updatedPlayer;
-
-//       return updatedPlayers;
-//     });
-//   };
-
-//   const handleAddPlayer = (name) =>  {
-//     setPlayers(prevState => {
-//       return [
-//         ...prevState, 
-//         {
-//           name,
-//           score: 0,
-//           id: id += 1
-//         }
-//       ]
-//     });    
-//   };
-
-//   const handleRemovePlayer = (id) => { 
-//     setPlayers( prevState => prevState.filter(p => p.id !== id) );
-//   };
-
   return (
     <CountriesContext.Provider value={{ 
       countries,
